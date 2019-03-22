2c5810f0d9f6bc80dc50b937afb173d2ced7ec13
anselclement <clem-nogare@hotmail.fr>
2019-03-22 11:34:00 +0100

    Modification tinymce.init

--- templates/base.php
+++ templates/base.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title><?= $title ?></title>
     <script src="https://cloud.tinymce.com/5/tinymce.min.js"></script>
-    <script>tinymce.init({ selector:'textarea',
+    <script>tinymce.init({ mode: "exact",
+                           elements : "contentArticle",
                            entity_encoding : 'raw',
-                           plugins: 'wordcount',
                            forced_root_block : false,
                            force_p_newlines : false });</script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
